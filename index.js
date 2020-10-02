6b77bd3131fa309181f1f709a797ac1d0965f93c
unknown <adam.p.gibbons@gmail.com>
2020-10-02 14:10:46 +0100

    Changed port back to 3000, opening port on DO

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ io.on('connection', (socket) => {
     console.log('a user connected');
 });
 
-http.listen(80, () => {
-    console.log('listening on *:80');
+http.listen(3000, () => {
+    console.log('listening on *:3000');
 });
