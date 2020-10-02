1e76dab583f833b18d2ad8396c5cd94be47d4d99
unknown <adam.p.gibbons@gmail.com>
2020-10-02 13:49:49 +0100

    I think it finally works

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.json());
+
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+
+    io.emit('hello', req.body);
+
+    console.log(req.headers) // Call your action on the request here
+    console.log(req.body) // Call your action on the request here
+    res.send('root')
+    res.status(200).end() // Responding is important
+
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
