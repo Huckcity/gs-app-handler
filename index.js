504109912ee0a9e498c6543823f541cd493ff183
unknown <adam.p.gibbons@gmail.com>
2020-10-08 11:55:39 +0100

    trying to fix public folder stuff

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
 
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var io = require('socket.io').listen(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -34,7 +34,3 @@ io.on('connection', (socket) => {
 http.listen(PORT, () => {
     console.log('[LISTENING] Connected and listening on post '+PORT)
 });
-
-// http.listen(3000, () => {
-//     console.log('listening on *:80');
-// });
